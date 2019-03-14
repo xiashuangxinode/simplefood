994e5df9226c4017d763d9b7a9fc6751baf8f878
unknown <xiashuangxi@hotmail.com>
2019-03-15 01:07:40 +0800

    's'

--- assets/javascript/script.js
+++
@@ -1,8 +0,0 @@
-/*window.onscroll = function(){
-	var top = document.documentElement.scrollTop || document.body.scrollTop;
-	if(top > 0){
-		var el = $("navbar");
-		console.log(el.className)
-	}
-
-}*/
