6a8a3537cbc5f29dc4737320acfa0e799b0dd464
unknown <xiashuangxi@hotmail.com>
2019-03-24 14:39:20 +0800

    '1'

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -103,6 +103,62 @@
 		}
 	}
 
+
+	window.search = (txt) => {
+
+		axios.get('/cookbooks.json')
+			.then( function(response){
+				var data = response.data;
+
+				for(var i=0 ; i <= data.length ; i++ ){
+					var cookbook = data[1];
+
+					if(cookbook.title.indexOf(txt) > 0){
+						console.log(cookbook.title)
+					}
+				}
+		});
+	}
+window.showMore = function(showEl,count){
+		var currentCount =parseInt($("#"+showEl+" li:last").attr('index'));
+		
+		var nextcount = currentCount + parseInt(count);
+
+		var loading_html = '<li id="loading" class="col-12 p-2 fiex-fill db-highlight d-flex justify-content-center "><div class="spinner-grow" role="status"><span class="sr-only">Loading...</span></div></li>';
+		$('#'+showEl).append(loading_html);
+
+
+
+		axios.get('/cookbooks.json')
+			.then( function(response){
+				var data = response.data.slice(currentCount,nextcount);
+				var index = currentCount;
+				for( var i=0; i<= data.length; i++){
+					var cookbook = data[i];
+
+					if(cookbook != undefined){
+						var index = index +1;
+					var element_html = '<li class="col-sm-6 col-md-6 col-lg-3 col-xl-3 p-2 fiex-fill db-highlight" index="'+ index +'"><div class="card"><a href="'+cookbook.url+'">';
+					  	element_html += '<img src="/assets/images/16-9.jpg" class="card-img-top" alt="'+cookbook.title+'">';
+					  	element_html += '</a><div class="card-body px-0">';
+					    element_html += '<a href="'+ cookbook.url+'"><h5 class="card-title">'+cookbook.title+'</h5></a>';
+					    element_html += '<p class="card-text text-dark font-weight-light">'+cookbook.excerpt+'</p>';
+					    element_html += '<div class="row mx-0 font-weight-light text-secondary size-85 ">';
+					    element_html += '<span class="col-6 p-0"><i class="far fa-clock mr-2"></i>'+ cookbook.minute +'分钟</span>';
+					    element_html += '<span class="col-6 p-0"><i class="fab fa-cloudscale mx-2"></i>'+ cookbook.kcal +'Kcal</span>';
+					    element_html += '</div></div></div></li>';
+
+					    $('#'+showEl).append(element_html);
+					}
+				}
+
+				$("#loading").remove();
+				if(parseInt($("#"+showEl+" li:last").attr('index')) == $("#"+showEl).attr("count") ){
+					$("#cookbooks-btn").hide();
+					$("#cookbooks-finish").show();
+				}
+			})
+	}
 	/*var eventBind = () =>{
 		$('[sf-nav-togger]').bind('click' , () => {
 			var tc = $('[sf-nav-togger]').attr('data-target');
--- search.md
+++ search.md
@@ -7,7 +7,7 @@ layout: simple_default
 		  <div class="input-group-prepend">
 		    <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-search"></i></span>
 		  </div>
-		  <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Recipient's username" aria-describedby="inputGroup-sizing-lg">
+		  <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Recipient's username" onkeyup="search($(this).val())" aria-describedby="inputGroup-sizing-lg">
 		</div>
 	</dir>
 </div>
